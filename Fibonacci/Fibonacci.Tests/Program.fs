bb238b31c6a715bf6276e68365954d4ea6962bbf
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-10-24 22:30:21 +0300

    implement function and tests

--- Fibonacci/Fibonacci.Tests/Program.fs
+++ Fibonacci/Fibonacci.Tests/Program.fs
@@ -4,13 +4,47 @@ open NUnit.Framework
 open FsUnit
 open Fibonacci
 
-[<SetUp>]
-let Setup () =
-    ()
+[<Test>]
+let ``Fibonacci of 0 should be 0`` () =
+    let result = fibonacci 0
+    result |> should equal (Ok 0I)
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let ``Fibonacci of 1 should be 1`` () =
+    let result = fibonacci 1
+    result |> should equal (Ok 1I)
+
+[<Test>]
+let ``Fibonacci of 2 should be 1`` () =
+    let result = fibonacci 2
+    result |> should equal (Ok 1I)
+
+[<Test>]
+let ``Fibonacci of 3 should be 2`` () =
+    let result = fibonacci 3
+    result |> should equal (Ok 2I)
+
+[<Test>]
+let ``Fibonacci of 4 should be 3`` () =
+    let result = fibonacci 4
+    result |> should equal (Ok 3I)
+
+[<Test>]
+let ``Fibonacci of 50 should work with bigint`` () =
+    let result = fibonacci 50
+    result |> should equal (Ok 12586269025I)
+
+[<Test>]
+let ``Fibonacci of 100 should work with bigint`` () =
+    let result = fibonacci 100
+    result |> should equal (Ok 354224848179261915075I)
+
+[<Test>]
+let ``Fibonacci of -1 should return Error`` () =
+    let result = fibonacci -1
+    match result with
+    | Error msg -> msg |> should equal "n must be non-negative integer"
+    | _ -> failwith "Should return Error"
 
 
 [<EntryPoint>]
--- Fibonacci/Fibonacci/Program.fs
+++ Fibonacci/Fibonacci/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Fibonacci
+
+
+/// Сomputes n-th fibonacci number
+let fibonacci n =
+    let rec calculate n a b =
+        match n with
+        | 0 -> Ok a
+        | 1 -> Ok b
+        | _ when n > 1 -> calculate (n - 1) b (a + b)
+        | _ -> Error "n must be non-negative integer"
+    calculate n 0I 1I
