91adea069e53523a212e5827690b9401b70873d1
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-10-24 22:27:13 +0300

    initial commit

---
+++ Fibonacci/Fibonacci.Tests/Program.fs
@@ -0,0 +1,17 @@
+﻿module Fibonacci.Tests
+
+open NUnit.Framework
+open FsUnit
+open Fibonacci
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
+
+
+[<EntryPoint>]
+let main _ = 0
