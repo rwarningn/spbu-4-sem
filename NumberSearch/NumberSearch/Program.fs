ea170a4e1a4f40bd41b1621fabaf19e5d92323a4
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-10-24 23:14:27 +0300

    feat: implement function and develop tests

--- NumberSearch/NumberSearch/Program.fs
+++ NumberSearch/NumberSearch/Program.fs
@@ -1,2 +1,10 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module NumberSearch
+
+/// returns the index of the first occurrence of the given element in the list
+let findFirst list element =
+    let rec find list element index =
+        match list with
+        | [] -> None
+        | head :: tail when head = element -> Some index
+        | _ :: tail -> find tail element (index + 1)
+    find list element 0
