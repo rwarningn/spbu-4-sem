1365da814c8221c9cdf5ae458cca65248ba95ae6
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-10-24 23:27:24 +0300

    feat: implement tests and lispower function

--- ListPower/ListPower/Program.fs
+++ ListPower/ListPower/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module ListPower
+
+/// returns a list [2^n; 2^(n+1); ...; 2^(n+m)]
+let powerList n m =
+    match m with
+    | m when m < 0 -> []
+    | _ ->
+        let rec buildList current count acc =
+            match count with
+            | 0 -> List.rev (current :: acc)
+            | _ -> buildList (current * 2I) (count - 1) (current :: acc)
+        buildList (1I <<< n) m []
