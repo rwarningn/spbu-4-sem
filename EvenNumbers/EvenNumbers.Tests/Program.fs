bbc337fd9af4aa8da84cdd290e9f6fec4331b5b0
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-10-25 11:52:31 +0300

    initial commit

---
+++ EvenNumbers/EvenNumbers.Tests/Program.fs
@@ -0,0 +1,17 @@
+﻿module EvenNumbers.Tests
+
+open NUnit.Framework
+open FsCheck
+open EvenNumbers
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
+
+
+[<EntryPoint>]
+let main _ = 0
