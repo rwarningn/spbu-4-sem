c70adb6ebda33882c2d5ee49780d6925f8e57fec
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-10-26 16:16:52 +0300

    implement solution and tests

--- PointFree/PointFree/Program.fs
+++ PointFree/PointFree/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module PointFree
+
+let func1 x l = List.map (fun y -> y * x) l
+
+let func2 x = List.map (fun y -> y * x)
+
+let func3 x = List.map ((*) x)
+
+let func4 = List.map << (*)
+
+let func5: int -> List<int> -> List<int> = 
+    (*) >> List.map
