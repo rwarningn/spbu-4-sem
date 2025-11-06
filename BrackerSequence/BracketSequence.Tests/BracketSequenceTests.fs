e02bcec6ed07dc0bfc5e2780072872594e934a25
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-11-06 23:18:58 +0300

    refactor: replace old duplicate tests with TestCase

--- BrackerSequence/BracketSequence.Tests/BracketSequenceTests.fs
+++ BrackerSequence/BracketSequence.Tests/BracketSequenceTests.fs
@@ -4,97 +4,31 @@ open NUnit.Framework
 open FsUnit
 open BracketSequence
 
-[<Test>]
-let ``Empty string is valid`` () =
-    checkBrackets "" |> should equal true
-
-[<Test>]
-let ``Single pair of round brackets`` () =
-    checkBrackets "()" |> should equal true
-
-[<Test>]
-let ``Single pair of square brackets`` () =
-    checkBrackets "[]" |> should equal true
-
-[<Test>]
-let ``Single pair of curly brackets`` () =
-    checkBrackets "{}" |> should equal true
-
-[<Test>]
-let ``Multiple pairs in sequence`` () =
-    checkBrackets "()[]{}" |> should equal true
-
-[<Test>]
-let ``Nested brackets`` () =
-    checkBrackets "({[]})" |> should equal true
-
-[<Test>]
-let ``Deeply nested brackets`` () =
-    checkBrackets "((((()))))" |> should equal true
-
-[<Test>]
-let ``Complex nested structure`` () =
-    checkBrackets "({()[]{}})" |> should equal true
-
-[<Test>]
-let ``Mismatched bracket types`` () =
-    checkBrackets "(]" |> should equal false
-
-[<Test>]
-let ``Wrong closing order`` () =
-    checkBrackets "([)]" |> should equal false
-
-[<Test>]
-let ``Extra closing bracket`` () =
-    checkBrackets "())" |> should equal false
-
-[<Test>]
-let ``Extra opening bracket`` () =
-    checkBrackets "(()" |> should equal false
-
-[<Test>]
-let ``Only opening bracket`` () =
-    checkBrackets "(" |> should equal false
-
-[<Test>]
-let ``Only closing bracket`` () =
-    checkBrackets ")" |> should equal false
-
-[<Test>]
-let ``Multiple unmatched opening`` () =
-    checkBrackets "(((" |> should equal false
-
-[<Test>]
-let ``Multiple unmatched closing`` () =
-    checkBrackets ")))" |> should equal false
-
-[<Test>]
-let ``String with other characters`` () =
-    checkBrackets "a(b[c]{d}e)f" |> should equal true
-
-[<Test>]
-let ``String with text and valid brackets`` () =
-    checkBrackets "func(x, arr[0], {key: val})" |> should equal true
-
-[<Test>]
-let ``String with text and invalid brackets`` () =
-    checkBrackets "func(x, arr[0}, {key: val)" |> should equal false
-
-[<Test>]
-let ``Spaces and brackets`` () =
-    checkBrackets "( [ { } ] )" |> should equal true
-
-[<Test>]
-let ``Interleaved valid and invalid`` () =
-    checkBrackets "{[}]" |> should equal false
-
-[<Test>]
-let ``Valid complex expression`` () =
-    checkBrackets "([{()}][])" |> should equal true
-
-[<Test>]
-let ``Invalid complex expression`` () =
-    checkBrackets "([{()][])" |> should equal false
+[<TestCase("", true)>]
+[<TestCase("()", true)>]
+[<TestCase("[]", true)>]
+[<TestCase("{}", true)>]
+[<TestCase("()[]{}", true)>]
+[<TestCase("({[]})", true)>]
+[<TestCase("((((()))))", true)>]
+[<TestCase("({()[]{}})", true)>]
+[<TestCase("(]", false)>]
+[<TestCase("([)]", false)>]
+[<TestCase("())", false)>]
+[<TestCase("(()", false)>]
+[<TestCase("(", false)>]
+[<TestCase(")", false)>]
+[<TestCase("(((", false)>]
+[<TestCase(")))", false)>]
+[<TestCase("a(b[c]{d}e)f", true)>]
+[<TestCase("func(x, arr[0], {key: val})", true)>]
+[<TestCase("func(x, arr[0}, {key: val)", false)>]
+[<TestCase("( [ { } ] )", true)>]
+[<TestCase("{[}]", false)>]
+[<TestCase("([{()}][])", true)>]
+[<TestCase("([{()][])", false)>]
+let ``checkBrackets returns correct result`` (input: string) (expected: bool) =
+    checkBrackets input |> should equal expected
 
 [<Test>]
 let ``Long valid sequence`` () =
