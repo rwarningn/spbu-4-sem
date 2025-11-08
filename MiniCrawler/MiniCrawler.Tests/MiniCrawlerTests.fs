d2175484daa2d08b3b84b063cce475be47835510
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-11-08 18:02:54 +0300

    feat: implement solution & tests

--- MiniCrawler/MiniCrawler.Tests/MiniCrawlerTests.fs
+++ MiniCrawler/MiniCrawler.Tests/MiniCrawlerTests.fs
@@ -1,11 +1,60 @@
-﻿module MiniCrawler.Tests
+﻿module MiniCrawlerTests
 
+open MiniCrawler
 open NUnit.Framework
+open FsUnit
+open System
+open System.IO
+open System.Net
+open System.Net.Http
+open RichardSzalay.MockHttp
 
-[<SetUp>]
-let Setup () =
-    ()
+[<TestFixture>]
+type ``MiniCrawler tests`` () =
 
-[<Test>]
-let Test1 () =
-    Assert.Pass()
+    // creates mock HttpClient
+    let makeMockClient (map: (string * string) list) =
+        let handler = new MockHttpMessageHandler()
+        for (url, content) in map do
+            handler.When(url).Respond(HttpStatusCode.OK, new StringContent(content)) |> ignore
+        handler.ToHttpClient()
+
+    [<Test>]
+    member _.``extractLinks finds only http links`` () =
+        let html = """
+            <a href="http://a.com">A</a>
+            <a href="https://b.com">B</a>
+            <a href="/relative">C</a>
+        """
+        extractLinks html |> should equal ["http://a.com"]
+
+    [<Test>]
+    member _.``extractLinks returns distinct results`` () =
+        let html = """<a href="http://x.com">x</a><a href="http://x.com">dup</a>"""
+        extractLinks html |> should equal ["http://x.com"]
+
+    [<Test>]
+    member _.``crawlAndPrintWith downloads linked pages and prints their lengths`` () =
+        let root = "http://root.com"
+        let c1 = "http://c1.com"
+        let c2 = "http://c2.com"
+        let rootHtml = $"""<a href="{c1}">1</a><a href="{c2}">2</a>"""
+
+        let client = makeMockClient [
+            root, rootHtml
+            c1, "AAA"
+            c2, "HelloWorld"
+        ]
+
+        use sw = new StringWriter()
+        let old = Console.Out
+        Console.SetOut(sw)
+
+        try
+            Async.RunSynchronously (crawlAndPrintWith client root)
+        finally
+            Console.SetOut(old)
+
+        let output = sw.ToString().Replace("\r\n", "\n")
+        output |> should contain (sprintf "%s - %d" c1 3)
+        output |> should contain (sprintf "%s - %d" c2 10)
--- MiniCrawler/MiniCrawler/Program.fs
+++ MiniCrawler/MiniCrawler/Program.fs
@@ -1,2 +1,42 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module MiniCrawler
+
+open System
+open System.Net.Http
+open System.Text.RegularExpressions
+
+
+/// extracts only http:// links from html
+let extractLinks (html: string) : string list = 
+    let rx = Regex("<a\\s+[^>]*href=\"(http://[^\"]+)\"", RegexOptions.IgnoreCase)
+    rx.Matches(html)
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> m.Groups.[1].Value)
+    |> Seq.distinct
+    |> Seq.toList
+
+/// downloads a page async using HttpClient
+let private download (client: HttpClient) (url: string) = async {
+    let! s = client.GetStringAsync(url) |> Async.AwaitTask
+    return s
+}
+
+/// downloads root page, extract links, download each in parallel and print sizes
+let crawlAndPrintWith (client: HttpClient) (url: string) = async {
+    let! main = download client url
+    let links = extractLinks main
+
+    let! results = 
+        links
+        |> List.map (fun u -> async {
+            let! content = download client u
+            return (u, content.Length)
+        })
+        |> Async.Parallel
+
+    results |> Array.iter (fun (u, len) -> printfn "%s - %d" u len)
+}
+
+/// wrapper that creates its own HttpClient
+let crawlAndPrint (url: string) = 
+    use client = new HttpClient()
+    crawlAndPrintWith client url
