fc64c7482b3f23cd893ce9e9add15bd663732177
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-11-08 17:23:24 +0300

    feat: implement solution & tests

--- Workflow/Workflow.Tests/WorkflowTests.fs
+++ Workflow/Workflow.Tests/WorkflowTests.fs
@@ -5,10 +5,72 @@ open NUnit.Framework
 open FsCheck
 open FsUnit
 
-[<SetUp>]
-let Setup () =
-    ()
 
-[<Test>]
-let Test1 () =
-    Assert.Pass()
+[<TestFixture>]
+type ``String calculator workflow tests`` () =
+
+    [<Test>]
+    member _.``Parses valid numbers correctly`` () =
+        let result =
+            calculator {
+                let! a = "10"
+                let! b = "20"
+                return (a + b)
+            }
+        result |> should equal (Some 30)
+
+    [<Test>]
+    member _.``Returns None for invalid input`` () =
+        let result =
+            calculator {
+                let! a = "5"
+                let! b = "xyz"
+                return a + b
+            }
+        result |> should equal None
+
+    [<Test>]
+    member _.``Performs multi-step calculation`` () =
+        let result =
+            calculator {
+                let! a = "2"
+                let! b = "3"
+                let! c = "4"
+                return (a + b) * c
+            }
+        result |> should equal (Some 20)
+
+
+[<TestFixture>]
+type ``Rounding workflow tests`` () =
+
+    [<Test>]
+    member _.``Basic example from assignment`` () =
+        let result =
+            rounding 3 {
+                let! a = 2.0 / 12.0
+                let! b = 3.5
+                return a / b
+            }
+        result |> should (equalWithin 1e-12) 0.048
+
+    [<Test>]
+    member _.``Zero precision rounds to integers`` () =
+        let result =
+            rounding 0 {
+                let! a = 3.1415
+                let! b = 2.71828
+                return a + b
+            }
+        result |> should equal 6.0
+
+    [<Test>]
+    member _.``Intermediate rounding affects result`` () =
+        let result =
+            rounding 1 {
+                let! a = 10.0 / 3.0
+                let! b = a * 2.0
+                let! c = b + 1.5
+                return c
+            }
+        result |> should (equalWithin 1e-12) 8.1
--- Workflow/Workflow/Program.fs
+++ Workflow/Workflow/Program.fs
@@ -1,2 +1,27 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Workflow
+
+open System
+
+// workflow for string-to-int calculations
+type StringCalculatorBuilder() =
+    member _.Bind(x: string, f: int -> int option) : int option =
+        match Int32.TryParse x with
+        | true, value -> f value
+        | false, _ -> None
+
+    member _.Return(x: int) : int option =
+        Some x
+
+let calculator = StringCalculatorBuilder()
+
+
+// workflow that performs intermediate rounding to given accuracy
+type RoundingBuilder(acc: int) =
+    member _.Bind(x: float, f: float -> float) : float =
+        let rounded = Math.Round(x, acc)
+        f rounded
+
+    member _.Return(x: float) : float =
+        Math.Round(x, acc)
+
+let rounding acc = RoundingBuilder(acc)
